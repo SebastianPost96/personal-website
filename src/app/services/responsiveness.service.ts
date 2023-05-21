e2fd514fd9a01d4e165bb31fee1d532153dbc418
Sebastian Post <sebastianpost@online.de>
2023-05-21 16:17:27 +0200

    add darkmode signal

--- src/app/services/responsiveness.service.ts
+++ src/app/services/responsiveness.service.ts
@@ -1,5 +1,9 @@
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Injectable, inject } from '@angular/core';
+import {
+  BreakpointObserver,
+  Breakpoints,
+  MediaMatcher,
+} from '@angular/cdk/layout';
+import { Injectable, inject, signal } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { map } from 'rxjs';
 
@@ -7,11 +11,23 @@ import { map } from 'rxjs';
   providedIn: 'root',
 })
 export class ResponsivenessService {
+  private darkModeQuery = inject(MediaMatcher).matchMedia(
+    '(prefers-color-scheme: dark)'
+  );
+
+  public isDarkMode = signal(this.darkModeQuery.matches);
   public isMobile = toSignal(
     inject(BreakpointObserver)
       .observe(Breakpoints.Handset)
       .pipe(map(({ matches }) => matches))
   );
+
+  constructor() {
+    this.darkModeQuery.addEventListener('change', (darkMode) => {
+      this.isDarkMode.set(darkMode.matches);
+      this.syncMobileHeaderColor();
+    });
+  }
 
   public syncMobileHeaderColor() {
     const toolbar = document.getElementById('toolbar');
