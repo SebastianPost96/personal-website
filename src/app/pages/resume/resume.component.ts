418954563641186d96f80e67ccbf6c62213a8e7d
Sebastian Post <sebastianpost@online.de>
2023-05-22 22:17:37 +0200

    add cv

--- src/app/pages/resume/resume.component.ts
+++ src/app/pages/resume/resume.component.ts
@@ -1,14 +1,21 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, HostBinding } from '@angular/core';
+import { PdfViewerModule } from 'ng2-pdf-viewer';
+import { ResponsivenessService } from 'src/app/services/responsiveness.service';
 
 @Component({
   selector: 'app-resume',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, PdfViewerModule],
   templateUrl: './resume.component.html',
   styleUrls: ['./resume.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ResumeComponent {
+  @HostBinding('style.padding')
+  get padding(): string {
+    return this.responsive.isMobile() ? '' : '20px 0';
+  }
 
+  constructor(public responsive: ResponsivenessService) {}
 }
