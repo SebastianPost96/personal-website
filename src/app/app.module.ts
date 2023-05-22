cbcf3f79ca4c9db8a8863e662dc82784aad6cf7c
Sebastian Post <sebastianpost@online.de>
2023-05-22 18:53:16 +0200

    refactor toolbar into component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,23 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MatButtonModule } from '@angular/material/button';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { HttpClientModule } from '@angular/common/http';
+import { ToolbarComponent } from './appframe/toolbar/toolbar.component';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatButtonModule,
     BrowserAnimationsModule,
     MatSidenavModule,
-    MatToolbarModule,
-    MatIconModule,
     MatListModule,
     HttpClientModule,
+    ToolbarComponent,
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/appframe/toolbar/toolbar.component.ts
@@ -0,0 +1,32 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Output,
+} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { ResponsivenessService } from 'src/app/services/responsiveness.service';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-toolbar',
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatDividerModule,
+    MatButtonModule,
+  ],
+  templateUrl: './toolbar.component.html',
+  styleUrls: ['./toolbar.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ToolbarComponent {
+  @Output() toggleSideNav = new EventEmitter<void>();
+
+  constructor(public responsive: ResponsivenessService) {}
+}
