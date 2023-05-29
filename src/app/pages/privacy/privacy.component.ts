2779ed9675ed3f50f0860c3433e0e03bbc1dad0e
Sebastian Post <sebastianpost@online.de>
2023-05-29 15:12:17 +0200

    add privacy policy

--- src/app/pages/privacy/privacy.component.ts
+++ src/app/pages/privacy/privacy.component.ts
@@ -1,14 +1,15 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-privacy',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './privacy.component.html',
   styleUrls: ['./privacy.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PrivacyComponent {
-
+  url = window.location.origin;
 }
