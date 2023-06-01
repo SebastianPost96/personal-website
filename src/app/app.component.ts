3726833602a0ed4bb3f04881b6b414cb0801d482
Sebastian Post <sebastianpost@online.de>
2023-06-01 17:02:41 +0200

    refactor to hostcontext

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostBinding, signal } from '@angular/core';
 
 import { IconsService } from './services/icons.service';
+import { ResponsivenessService } from './services/responsiveness.service';
 
 @Component({
   selector: 'app-root',
@@ -9,9 +10,17 @@ import { IconsService } from './services/icons.service';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
+  @HostBinding('class')
+  get rootClass() {
+    const theme = this.responsive.isDarkMode() ? 'dark' : 'light';
+    const device = this.responsive.isMobile() ? 'mobile' : 'desktop';
+
+    return `${theme} ${device}`;
+  }
+
   public sideNavOpen = signal(false);
 
-  constructor(private icons: IconsService) {}
+  constructor(private icons: IconsService, private responsive: ResponsivenessService) {}
 
   toggleSideNav(explicit?: boolean): void {
     this.sideNavOpen.update((open) => (typeof explicit === 'boolean' ? explicit : !open));
--- src/app/services/responsiveness.service.ts
+++ src/app/services/responsiveness.service.ts
@@ -1,6 +1,6 @@
 import { BreakpointObserver, Breakpoints, MediaMatcher } from '@angular/cdk/layout';
-import { Injectable, WritableSignal, effect, inject, signal } from '@angular/core';
-import { toSignal } from '@angular/core/rxjs-interop';
+import { inject, Injectable, signal } from '@angular/core';
+import { takeUntilDestroyed, toObservable, toSignal } from '@angular/core/rxjs-interop';
 import { map } from 'rxjs';
 
 const DARK_MODE = 'darkmode';
@@ -17,6 +17,7 @@ export class ResponsivenessService {
   );
 
   constructor() {
+    this.syncMobileHeaderColor();
     this.loadAndPersistDarkMode();
   }
 
@@ -27,23 +28,20 @@ export class ResponsivenessService {
   }
 
   private syncMobileHeaderColor() {
-    const toolbar = document.getElementById('toolbar');
-    const headerElem = document.getElementById('theme-color');
+    toObservable(this.isDarkMode)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => {
+        const toolbar = document.getElementById('toolbar');
+        const headerElem = document.getElementById('theme-color');
 
-    if (toolbar && headerElem) {
-      const color = getComputedStyle(toolbar).backgroundColor;
-      headerElem.setAttribute('content', color);
-    }
+        if (toolbar && headerElem) {
+          const color = getComputedStyle(toolbar).backgroundColor;
+          headerElem.setAttribute('content', color);
+        }
+      });
   }
 
   private loadAndPersistDarkMode(): void {
-    effect(() => {
-      const body = document.getElementsByTagName('body')[0];
-      this.isDarkMode() ? body.classList.add('dark') : body.classList.remove('dark');
-
-      this.syncMobileHeaderColor();
-    });
-
     const stored = localStorage.getItem(DARK_MODE);
     if (stored) {
       this.isDarkMode.set(stored === 'true');
