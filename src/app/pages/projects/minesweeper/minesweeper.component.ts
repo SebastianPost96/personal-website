4fb642d7ca62abff5a8d9f1f24106cbee88d1ebf
Sebastian Post <sebastianpost@online.de>
2023-06-01 22:42:17 +0200

    add minimum loading time for MS

--- src/app/pages/projects/minesweeper/minesweeper.component.ts
+++ src/app/pages/projects/minesweeper/minesweeper.component.ts
@@ -1,9 +1,10 @@
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { BehaviorSubject, combineLatest, map, shareReplay, timer } from 'rxjs';
 
 @Component({
   selector: 'app-minesweeper',
@@ -14,5 +15,11 @@ import { MatTooltipModule } from '@angular/material/tooltip';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MinesweeperComponent {
-  loading = signal(true);
+  hasData$ = new BehaviorSubject(false);
+  minimumLoadTime$ = timer(500);
+
+  hasLoaded$ = combineLatest([this.hasData$, this.minimumLoadTime$]).pipe(
+    map(([hasData]) => hasData),
+    shareReplay(0)
+  );
 }
