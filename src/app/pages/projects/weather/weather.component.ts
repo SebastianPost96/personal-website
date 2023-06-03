d71ebad4f31bb4e758ce99553976552605bfd9a7
Sebastian Post <sebastianpost@online.de>
2023-06-03 18:03:41 +0200

    add showcase for weather app

--- src/app/pages/projects/weather/weather.component.ts
+++ src/app/pages/projects/weather/weather.component.ts
@@ -1,14 +1,18 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { ResponsivenessService } from '../../../services/responsiveness.service';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-weather',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatTooltipModule, NgOptimizedImage, MatButtonModule, MatIconModule],
   templateUrl: './weather.component.html',
   styleUrls: ['./weather.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class WeatherComponent {
-
+  responsive = inject(ResponsivenessService);
 }
