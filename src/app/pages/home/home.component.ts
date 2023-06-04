72149059cf98ee41b4be6358321f781824ea0d6c
Sebastian Post <sebastianpost@online.de>
2023-06-04 19:49:05 +0200

    add home data

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
-type Skill = {
+type Proficiency = {
   icon: string;
   label: string;
   experience: 1 | 2 | 3 | 4 | 5;
 };
 
 type Reference = {
+  icon: string;
+  label: string;
+};
+
+type Tool = {
   icon: string;
   label: string;
 };
@@ -21,7 +26,43 @@ type Reference = {
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeComponent {
-  public readonly skills: Record<string, Skill[]> = { Languages: [], Frameworks: [], 'UI Libraries': [] };
+  public readonly proficiencies: Record<string, Proficiency[]> = {
+    Languages: [
+      { label: 'TypeScript', icon: '', experience: 5 },
+      { label: 'JavaScript', icon: '', experience: 5 },
+      { label: 'HTML', icon: '', experience: 4 },
+      { label: 'SCSS', icon: '', experience: 3 },
+      { label: 'C#', icon: '', experience: 2 },
+    ],
+    Frameworks: [
+      { label: 'Angular', icon: '', experience: 5 },
+      { label: 'Nx', icon: '', experience: 3 },
+      { label: 'Ionic', icon: '', experience: 2 },
+      { label: 'React', icon: '', experience: 2 },
+      { label: 'NestJS', icon: '', experience: 2 },
+    ],
+    'UI Libraries': [
+      { label: 'Material', icon: '', experience: 4 },
+      { label: 'PrimeNG', icon: '', experience: 4 },
+      { label: 'Bootstrap', icon: '', experience: 3 },
+      { label: 'Tailwind', icon: '', experience: 2 },
+    ],
+    Skills: [
+      { label: 'Reactive Programming', icon: '', experience: 5 },
+      { label: 'Unit Testing', icon: '', experience: 3 },
+      { label: 'Progressive Web Apps', icon: '', experience: 3 },
+      { label: 'DevOps', icon: '', experience: 3 },
+      { label: 'SEO', icon: '', experience: 2 },
+    ],
+  };
 
   public readonly references: Reference[] = [];
+
+  public readonly tools: Tool[] = [
+    { label: 'WebStorm', icon: '' },
+    { label: 'Docker', icon: '' },
+    { label: 'GitHub', icon: '' },
+    { label: 'Windows', icon: '' },
+    { label: 'Insomnia', icon: '' },
+  ];
 }
